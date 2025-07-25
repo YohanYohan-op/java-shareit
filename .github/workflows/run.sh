56c7701af9505ae2c949610386fbe4416df11821
Слава <korolslava0@gmail.com>
2025-07-25 08:09:03 +0700

    fix pom xml 4

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,8 +1,8 @@
-mvn spring-boot:run &
+chmod a+x ./tests/.github/workflows/wait-for-it.sh
+
+java -jar ./target/shareit-0.0.1-SNAPSHOT.jar &
 
 APP_PID=$!
-
-chmod a+x ./tests/.github/workflows/wait-for-it.sh
 
 ./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080
 
