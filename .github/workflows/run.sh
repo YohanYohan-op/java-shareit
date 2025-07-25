1628d89dd64a3a0b4470c539d0a2dc3c4c74eef5
Слава <korolslava0@gmail.com>
2025-07-25 08:01:35 +0700

    fix pom xml 4

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,3 +1,11 @@
-mvn spring-boot:run
+mvn spring-boot:run &
+
+APP_PID=$!
+
 chmod a+x ./tests/.github/workflows/wait-for-it.sh
+
 ./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080
+
+netstat -tulnp || true
+
+wait $APP_PID
