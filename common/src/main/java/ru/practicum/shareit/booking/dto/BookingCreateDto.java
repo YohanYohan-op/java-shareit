2babe4bdd54fa0fc3fa99e67c23ad643a6b3109e
Слава <korolslava0@gmail.com>
2025-08-29 13:41:42 +0700

    add item requests and gateway

--- common/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ common/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
